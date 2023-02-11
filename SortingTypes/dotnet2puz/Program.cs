aceda917459f4757439f7c544d1f6592cce27a8e
Marat <msshayapov@gmail.com>
2023-02-11 21:48:13 +0100

    commit #2. решение задачи методом пузырька. вариант 1

--- SortingTypes/dotnet2puz/Program.cs
+++ SortingTypes/dotnet2puz/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите количество элементов массива: ");
+
+int n = Convert.ToInt32(ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Write("Введите значение массива: ");
+    array[i] = Convert.ToInt32(ReadLine());
+}
+WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    WriteLine(i + "[" + string.Join(", ", array) + "]");
+}
