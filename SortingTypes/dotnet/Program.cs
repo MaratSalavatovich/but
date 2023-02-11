09c08e75c0e0bc930df04882a68f37fd94b8f8eb
Marat <msshayapov@gmail.com>
2023-02-11 17:04:42 +0100

    commit #3. добавлен простой пример реализации сортировки выбором в С#

--- SortingTypes/dotnet/Program.cs
+++ SortingTypes/dotnet/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void SortType(int[] collection)
+{
+    int size = collection.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+        for (int j = i; j < size; j++)
+        {
+            if (collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp;
+    }
+}
+
+
+int[] array = { 7, 6, 3, 4, 5, 1, 2, 3 };
+
+Console.WriteLine($"[{String.Join(',', array)}]");
+SortType(array);
+Console.WriteLine($"[{String.Join(',', array)}]");
